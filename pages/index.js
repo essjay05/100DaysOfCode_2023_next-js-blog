7c2ba70d1da4b439da51c89fe476e6a2e85cb278
Joy Serquiña <stepjoyous@gmail.com>
2023-04-05 00:25:26 -0700

    Implemented projects retrieval for getStaticProps successfully.

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,44 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+// import { getSortedPostsData } from '../lib/posts'
+// import { getProjectsData } from '../lib/projects'
 
-export default function Home({ allPostsData }) {
+export default function Home({ projects }) {
+
+  console.log('Index.js allProjectsData:')
+  console.log(projects)
+
+  const projectsData = projects.data
+
+  // const projectsData = allProjectsData.data
+  
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>I'm a Joy-of-All-Trades former healthcare worker and theatre stage manager, turned software engineer.</p>
       </section>
+      {/* Projects */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {projectsData.map((project) => {
+            const { id, attributes } = {...project}
+            const { name, description } = {...attributes}
+            return (
+              <li className={utilStyles.listItem} key={id}>
+                <h3>{name}</h3>
+                <p>{description}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+      {/* Blog */}
+      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -25,16 +51,22 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
-      </section>
+      </section> */}
     </Layout>
   )
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  // const allPostsData = getSortedPostsData()
+  const url = `https://find-your-joy-app.onrender.com/api/projects`
+  const allProjectsData = await fetch(url)
+  // const projectsJson = await allProjectsData.json()
+  console.log('allProjectsData')
+  console.log(allProjectsData)
   return {
     props: {
-      allPostsData
+      projects: await allProjectsData.json()
     }
   }
 }
+
