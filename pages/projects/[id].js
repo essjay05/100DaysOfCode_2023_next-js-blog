a992996dcf72a8320f15646497ba3b968cbbbbcb
Joy Serquiña <stepjoyous@gmail.com>
2023-04-04 23:38:02 -0700

    Created base for /pages/projects dynamic routing for project ids.

---
+++ pages/projects/[id].js
@@ -0,0 +1,15 @@
+import Layout from '../../components/layout'
+
+export default function Project() {
+  return (
+    <Layout></Layout>
+  )
+}
+
+// export async function getStaticPaths() {
+
+// }
+
+// export async function getStaticProps() {
+
+// }
