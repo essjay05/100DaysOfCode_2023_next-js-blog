acdf33b935bb8d658bc0112b3ee4e6f4b508750f
Joy Serquiña <stepjoyous@gmail.com>
2023-04-08 14:12:32 -0700

    Installed date-fns package to help process date formations.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,8 @@
 import Layout from '../../components/layout';
 import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -25,11 +27,14 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
+      <article>
+        <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+        div.
+      </article>
+      
+
       <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Date dateString={postData.date}/>
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}/>
     </Layout>
   );
